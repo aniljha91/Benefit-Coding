c812e634c3c1cc3f80b3c31a7b39b665d7070c70
Anil Jha <aniljha_91@yahoo.co.in>
2017-07-24 18:02:39 +0530

    creating benefit coding repository

---
+++ src/main/java/com/ccsp/service/UserService.java
@@ -0,0 +1,11 @@
+package com.ccsp.service;
+
+import java.util.List;
+
+import com.ccsp.entity.User;
+
+public interface UserService {
+	
+	List<User> findById();
+	
+}
---
+++ src/test/java/com/org/group/AccountServiceApplicationTests.java
@@ -0,0 +1,22 @@
+/*package com.ccsp.group;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.ccsp.BenefitCodingApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebMvcTest(BenefitCodingApplication.class)
+@WebAppConfiguration
+public class AccountServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+
+	}
+
+}
+*/
