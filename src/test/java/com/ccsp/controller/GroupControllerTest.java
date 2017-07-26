12e4f8a9e34595487cf1540f06830626a1f30742
Anil Jha <aniljha_91@yahoo.co.in>
2017-07-26 17:25:46 +0530

    Test for GroupController

---
+++ src/test/java/com/ccsp/controller/GroupControllerTest.java
@@ -0,0 +1,40 @@
+package com.ccsp.controller;
+
+
+import static org.mockito.Mockito.when;
+
+import java.net.UnknownHostException;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+
+public class GroupControllerTest {
+	
+	@InjectMocks
+	private GroupController groupController;
+	
+	@Mock
+	private RestTemplate restTemplate;
+	
+	@Test
+	public void testGetName() throws Exception {
+		String name = "test";
+		Assert.assertEquals(groupController.getName(name), name);
+	}
+	
+	@Test
+	public void testGetLoadBalancerDemo() throws UnknownHostException {
+		when(restTemplate.getForObject("http://BENEFIT-DETERMINATION/determine/plan", String.class)).thenReturn("Test");
+		String result = groupController.getLoadBalancerDemo();
+		String expected = "Inside Benefit coding service    ::   Instance used of BENEFIT-CODING is running on host :USBLRSKATHAVAR1    ::   And Internally called BENEFIT-DETERMINATION Service, Test";
+		Assert.assertEquals(expected, result);
+	}
+
+}
